5e6d9317ddd3148c721567e2f8bf7000a7d3479d
Eduardo <eduardo.hoths@gmail.com>
2024-07-25 22:32:25 -0300

    Editor configurado

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,10 @@ package application;
 import chess.ChessMatch;
 
 public class Program {
-    public static void main(String[] args) throws Exception {
-        ChessMatch chessMatch = new ChessMatch();
+  public static void main(String[] args) throws Exception {
+    ChessMatch chessMatch = new ChessMatch();
 
-        UI.printBoard(chessMatch.getPieces());
+    UI.printBoard(chessMatch.getPieces());
 
-    }
+  }
 }
